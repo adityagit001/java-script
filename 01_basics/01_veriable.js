0862f1624972808a555ee7851800feda06ed07d9
adityagit001 <adi9576tya@gmail.com>
2025-05-30 06:35:24 +0000

    commit variable

---
+++ 01_basics/01_veriable.js
@@ -0,0 +1,10 @@
+const accountId = 14562
+let accountEmail = "aditya@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountstate;
+// accountId = "2"
+accountEmail = "hc@gmail.com"
+accountPassword = "293884"
+accountCity = "Bangaluru"
+console.table([accountEmail,,accountPassword,accountCity,accountstate,accountId])
