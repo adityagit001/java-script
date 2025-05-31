4750e94f8bfc1246b5a3fde72df16d778d29f00a
adityagit001 <adi9576tya@gmail.com>
2025-05-31 09:08:26 +0000

    conversion of datatype

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "23"
+console.log(typeof score)
+
+let valueInNumber = Number(score);
+console.log(typeof score)
+console.log(typeof valueInNumber)
+
+//"23" => Number
+// "33abc" => Nan
+// true => 1; fales => 0
+
+let isLoggedIn = 1
+
+let booleanISLoggedIn = Boolean(isLoggedIn)
+console.log(booleanISLoggedIn)
+
+// 1 => ture ; 0 => false
+// "aditya" => ture
+// "" => false
