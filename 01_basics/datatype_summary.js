6fe64e8ddcfef4a5eb0f7ac3b6b497e879d87b4c
adityagit001 <adi9576tya@gmail.com>
2025-06-02 02:11:11 +0000

    datatype_summary

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -7,3 +7,29 @@
 // refrence(Non-primitive)
 
 // Array , Objects , Function
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (primitive), Heap(non-primitive)
+
+let myYouTubeName = "adityagamer"
+
+let anotherName = myYouTubeName
+anotherName = "mohitgamer"
+
+console.log(anotherName)
+console.log(myYouTubeName)
+
+
+let myDetal = {
+    email : "aditya@gmail.com",
+   upi : "aditya@ybl"
+
+}
+console.log(myDetal)
+
+let secMyDetal = myDetal;
+secMyDetal.upt = "mohit@ybl"
+
+console.log(myDetal.upi)
+console.log(secMyDetal.upi)
